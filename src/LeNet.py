d6bfb358b231c7a4568ffcb28c4e60625f0cbd50
YuHang0121151624 <yuhang0829@163.com>
2018-11-01 14:14:13 +0800

    Update src/LeNet.py

--- src/LeNet.py
+++ src/LeNet.py
@@ -1,236 +1,106 @@
-import torch 
-import torch.nn as nn
-from torch.autograd import Variable
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.utils import save_image  
-from pathlib import Path
-import matplotlib.pyplot as plt
-import numpy
-import pickle
-
-toImg = transforms.ToPILImage()
-
-# Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-# Hyper parameters
-num_epochs = 5
-num_classes = 10
-batch_size = 100
-learning_rate = 0.01
-eps = 0.3
-output_dir = './img/'
-# MNIST dataset
-train_dataset = torchvision.datasets.MNIST(root='./data/',
-                                           train=True, 
-                                           transform=transforms.ToTensor(),
-                                           download=False)
-
-test_dataset = torchvision.datasets.MNIST(root='./data/',
-                                          train=False, 
-                                          transform=transforms.ToTensor())
-
-# Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
-                                           shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
-                                          shuffle=False)
-
-# Convolutional neural network (two convolutional layers)
-class LeNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(LeNet, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7*7*32, num_classes)
-        
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-model = LeNet(num_classes).to(device)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-# Train the model
-def train():
-    total_step = len(train_loader)
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
-            
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            if (i+1) % 200 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                       .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-    # Save the model checkpoint
-    torch.save(model.state_dict(), 'model.pth')
-
-
-# Test the model
-def test():
-    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
-    with torch.no_grad(): # cut off gradient
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
-    # Save the model checkpoint
-    torch.save(model.state_dict(), 'model.pth')
-    
-    model.train()
-
-'''# this function still has bugs
-
-def fgsm_generate():
-    correct = 0
-    total = 0
-    correct_adv = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        images.requires_grad_()
-        outputs = model(images)
-        loss = criterion(outputs,labels)
-        
-        #loss.backward()
-        _,prediction = torch.max(outputs,1)
-
-        #FGSM
-        images_grad = torch.sign(images.grad)
-        images_adv = torch.clamp(images.detach() + eps*images_grad, 0, 1)
-        outputs_adv = model(images)
-        _,prediction_adv = torch.max(outputs_adv.data,1)
-        total += labels.size(0)
-        correct_adv += (prediction_adv == labels).sum().item()
-        correct += (prediction == labels).sum().item()
-
-
-    print('Before FGSM Test Accuracy on the 10000 test images: {} %'.format(100 * correct / total))
-    print('After FGSM Test Accuracy on the 10000 test images : {} %'.format(100 * correct_adv / total))
-            
+'''
+@author: liuaishan
+@contact: liuaishan@buaa.edu.cn
+@file: LeNet.py.py
+@time: 2018/10/29 15:54
+@desc:
 '''
 
-def fgsm():
-    model.eval()
+from torch.autograd import variable
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-    images_all = list()
-    adv_all = list()
-    correct = 0
-    correct_cln = 0
-    correct_adv = 0
-    total=0
-    for images,labels in test_loader:
-        x = Variable(images, requires_grad = True).to(device)
-        y_true = Variable(labels, requires_grad = False).to(device)
+class LeNet(nn.Module):
+    def __init__(self, enable_lat, epsilon, pro_num, batch_size=64,  batch_norm=False, if_dropout=False, class_num = 10):
+        # use the batch_norm after conv layer & use dropout after linear layer
+        super(LeNet, self).__init__()
+        self.train(True)
+        self.batch_norm = batch_norm
+        self.if_dropout = if_dropout
+        self.bn1 = nn.BatchNorm2d(1)
+        self.register_buffer('x_reg', torch.zeros([batch_size, 1, 28, 28]))
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5,
+                               padding=2)  # output_size:(batch_size,6,28,28)
+        self.bn2 = nn.BatchNorm2d(6)
+        self.register_buffer('z1_reg', torch.zeros([batch_size, 6, 28, 28]))
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)  # output_size:(batch_size,16,10,10)
+        self.bn3 = nn.BatchNorm2d(16)
+        self.register_buffer('z2_reg', torch.zeros([batch_size, 16, 10, 10]))
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.register_buffer('z3_reg', torch.zeros([batch_size, 120]))
+        self.fc2 = nn.Linear(120, 84)
+        self.register_buffer('z4_reg', torch.zeros([batch_size, 84]))
+        self.fc3 = nn.Linear(84, class_num)
 
-        h = model(x)
-        _, predictions = torch.max(h,1)
-        correct_cln += (predictions == y_true).sum()
-        loss = criterion(h, y_true)
-        model.zero_grad()
-        if x.grad is not None:
-            x.grad.data.fill_(0)
-        loss.backward()
-        
-        #FGSM
-        #x.grad.sign_()   # change the grad with sign ?
-        x_adv = x.detach() + eps * torch.sign(x.grad)
-        x_adv = torch.clamp(x_adv,0,1)
-        
-        h_adv = model(x_adv)
-        _, predictions_adv = torch.max(h_adv,1)
-        correct_adv += (predictions_adv == labels).sum()
+        self.enble_lat = enable_lat
+        self.epsilon = epsilon
+        self.pro_num = pro_num
+    def forward(self, x):
+                # layer 0
+        self.input = x
+        if (self.batch_norm):
+            self.input = self.bn1(self.input)
+        if (self.enble_lat):
+            self.input.retain_grad()
+            # LAT add saved grad to x_reg
+            input_add = self.input.add(self.epsilon / self.pro_num * torch.sign(self.x_reg.data))
+        else:
+            input_add = self.input
 
-        images_all.append([x.data.view(-1,28,28).detach().cpu(), labels])
-        adv_all.append([x_adv.data.view(-1,28,28).cpu(), predictions_adv])
+        # layer 1
+        self.z1 = self.conv1(input_add)
+        if (self.batch_norm):
+            self.z1 = self.bn2(self.z1)
+        if (self.enble_lat):
+            self.z1.retain_grad()
+            # LAT add saved grad to z1_reg
+            z1_add = self.z1.add(self.epsilon / self.pro_num * torch.sign(self.z1_reg.data))
+        else:
+            z1_add = self.z1
+        a1 = F.relu(z1_add)
+        p1 = F.max_pool2d(a1, (2, 2))
 
-        correct += (predictions == predictions_adv).sum()
-        total += len(predictions)
-    
-    model.train()
-    error_rate = float(total-correct)*100/total
-    print("Error Rate is ",float(total-correct)*100/total)
-    print("Before FGSM the accuracy is",float(100*correct_cln)/total)
-    print("After FGSM the accuracy is",float(100*correct_adv)/total)
+        # layer 2
+        self.z2 = self.conv2(p1)
+        if (self.batch_norm):
+            self.z2 = self.bn3(self.z2)
+        if (self.enble_lat):
+            self.z2.retain_grad()
+            # LAT add saved grad to z2_reg
+            z2_add = self.z2.add(self.epsilon / self.pro_num * torch.sign(self.z2_reg.data))
+        else:
+            z2_add = self.z2
+        a2 = F.relu(z2_add)
+        p2 = F.max_pool2d(a2, (2, 2))
 
-    return images_all, adv_all, error_rate
+        # layer 3
+        # do the LAT process before relu function
+        self.z3 = self.fc1(p2.view(p2.size(0), -1))
+        if (self.if_dropout):
+            self.z3 = F.dropout(self.z3, p=0.5, training=self.training)
+        if (self.enble_lat):
+            self.z3.retain_grad()
+            # LAT add saved grad to z3_reg
+            z3_add = self.z3.add(self.epsilon / self.pro_num * torch.sign(self.z3_reg.data))
+        else:
+            z3_add = self.z3
+        a3 = F.relu(z3_add)
 
-def save(images_all, adv_all):
-    #save adversarial examples
-    image, label = images_all[0]
-    image_adv, label_adv = adv_all[0]
-    tot = len(image)
-    for i in range(0, tot):
-        im = toImg(image[i].unsqueeze(0))
-        im.save(Path('img/eps_{}/{}_clean.jpg'.format(eps,i)))
-        im = toImg(image_adv[i].unsqueeze(0))
-        im.save(Path('img/eps_{}/{}_adver.jpg'.format(eps,i)))
+        # layer 4
+        # do the LAT process before relu function
+        self.z4 = self.fc2(a3)
+        if (self.if_dropout):
+            self.z4 = F.dropout(self.z4, p=0.5, training=self.training)
+        if (self.enble_lat):
+            self.z4.retain_grad()
+            # LAT add saved grad to z4_reg
+            z4_add = self.z4.add(self.epsilon / self.pro_num * torch.sign(self.z4_reg.data))
+        else:
+            z4_add = self.z4
+        a4 = F.relu(z4_add)
 
-def display(images_all, adv_all):
-    # display a batch adv
-    curr, label = images_all[0]
-    curr_adv, label_adv = adv_all[0]
-    disp_batch = 10
-    for a in range(disp_batch):
-        plt.figure()
-        plt.subplot(121)
-        plt.title('Original Label: {}'.format(label[a].cpu().numpy()),loc ='left')
-        plt.imshow(curr[a].numpy(),cmap='gray')
-        plt.subplot(122)
-        plt.title('Adv Label : {}'.format(label_adv[a].cpu().numpy()),loc ='left')
-        plt.imshow(curr_adv[a].numpy(),cmap='gray')
-        plt.show()
-    total=batch_size
-    correct=(label==label_adv).sum()
-    print("Batch Error rate ",float(total-correct)*100/total)
+        # layer 5
+        logits = self.fc3(a4)
 
-def draw(error_rate = error_rate, ):
-
-
-#train()
-model.load_state_dict(torch.load('model.pth'))
-#test()
-images_all, adv_all, error_rate = fgsm()
-save(images_all, adv_all)
-#display(images_all, adv_all)
-
-
-with open('clean.p','wb') as f:
-	pickle.dump(images_all[0], f, pickle.HIGHEST_PROTOCOL)
-
-with open('eps_{}.p'.format(eps),'wb') as f:
-	pickle.dump(adv_all[0], f, pickle.HIGHEST_PROTOCOL)
+        return logits, a4
