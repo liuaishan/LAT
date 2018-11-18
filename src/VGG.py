remove torch.sign() and fix bugs

src/VGG.py
@@ -28,7 +28,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.input.retain_grad()
             # LAT add saved grad to x_reg
-            input_add = self.input.add(self.epsilon / self.pro_num * torch.sign(self.x_reg.data))
+            input_add = self.input.add(self.epsilon / self.pro_num * self.x_reg.data)
         else:
             input_add = self.input
 
@@ -36,7 +36,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z1.retain_grad()
             # LAT add saved grad to z1_reg
-            z1_add = self.z1.add(self.epsilon / self.pro_num * torch.sign(self.z1_reg.data))
+            z1_add = self.z1.add(self.epsilon / self.pro_num * self.z1_reg.data)
         else:
             z1_add = self.z1
         a1 = self.features[2](self.features[1](z1_add)) #bn1,relu
@@ -45,7 +45,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z2.retain_grad()
             # LAT add saved grad to z2_reg
-            z2_add = self.z2.add(self.epsilon / self.pro_num * torch.sign(self.z2_reg.data))
+            z2_add = self.z2.add(self.epsilon / self.pro_num * self.z2_reg.data)
         else:
             z2_add = self.z2
         a2 = self.features[5](self.features[4](z2_add)) #bn2,relu
@@ -56,7 +56,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z3.retain_grad()
             # LAT add saved grad to z3_reg
-            z3_add = self.z3.add(self.epsilon / self.pro_num * torch.sign(self.z3_reg.data))
+            z3_add = self.z3.add(self.epsilon / self.pro_num * self.z3_reg.data)
         else:
             z3_add = self.z3
         a3 = self.features[9](self.features[8](z3_add)) #bn3,relu
@@ -65,7 +65,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z4.retain_grad()
             # LAT add saved grad to z4_reg
-            z4_add = self.z4.add(self.epsilon / self.pro_num * torch.sign(self.z4_reg.data))
+            z4_add = self.z4.add(self.epsilon / self.pro_num * self.z4_reg.data)
         else:
             z4_add = self.z4
         a4 = self.features[12](self.features[11](z4_add)) #bn2,relu
@@ -76,7 +76,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z5.retain_grad()
             # LAT add saved grad to z5_reg
-            z5_add = self.z5.add(self.epsilon / self.pro_num * torch.sign(self.z5_reg.data))
+            z5_add = self.z5.add(self.epsilon / self.pro_num * self.z5_reg.data)
         else:
             z5_add = self.z5
         a5 = self.features[16](self.features[15](z5_add)) #bn5,relu
@@ -85,7 +85,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z6.retain_grad()
             # LAT add saved grad to z6_reg
-            z6_add = self.z6.add(self.epsilon / self.pro_num * torch.sign(self.z6_reg.data))
+            z6_add = self.z6.add(self.epsilon / self.pro_num * self.z6_reg.data)
         else:
             z6_add = self.z6
         a6 = self.features[19](self.features[18](z6_add)) #bn6,relu
@@ -94,7 +94,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z7.retain_grad()
             # LAT add saved grad to z7_reg
-            z7_add = self.z7.add(self.epsilon / self.pro_num * torch.sign(self.z7_reg.data))
+            z7_add = self.z7.add(self.epsilon / self.pro_num * self.z7_reg.data)
         else:
             z7_add = self.z7
         a7 = self.features[22](self.features[21](z7_add)) #bn7,relu
@@ -105,7 +105,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z8.retain_grad()
             # LAT add saved grad to z8_reg
-            z8_add = self.z8.add(self.epsilon / self.pro_num * torch.sign(self.z8_reg.data))
+            z8_add = self.z8.add(self.epsilon / self.pro_num * self.z8_reg.data)
         else:
             z8_add = self.z8
         a8 = self.features[26](self.features[25](z8_add)) #bn8,relu
@@ -114,7 +114,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z9.retain_grad()
             # LAT add saved grad to z9_reg
-            z9_add = self.z9.add(self.epsilon / self.pro_num * torch.sign(self.z9_reg.data))
+            z9_add = self.z9.add(self.epsilon / self.pro_num * self.z9_reg.data)
         else:
             z9_add = self.z9
         a9 = self.features[29](self.features[28](z9_add)) #bn2,relu
@@ -123,7 +123,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z10.retain_grad()
             # LAT add saved grad to z10_reg
-            z10_add = self.z10.add(self.epsilon / self.pro_num * torch.sign(self.z10_reg.data))
+            z10_add = self.z10.add(self.epsilon / self.pro_num * self.z10_reg.data)
         else:
             z10_add = self.z10
         a10 = self.features[32](self.features[31](z10_add)) #bn10,relu
@@ -134,7 +134,7 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z11.retain_grad()
             # LAT add saved grad to z11_reg
-            z11_add = self.z11.add(self.epsilon / self.pro_num * torch.sign(self.z11_reg.data))
+            z11_add = self.z11.add(self.epsilon / self.pro_num * self.z11_reg.data)
         else:
             z11_add = self.z11
         a11 = self.features[36](self.features[35](z11_add)) #bn11,relu
@@ -143,19 +143,19 @@ class VGG16(nn.Module):
         if self.enable_lat:
             self.z12.retain_grad()
             # LAT add saved grad to z12_reg
-            z12_add = self.z12.add(self.epsilon / self.pro_num * torch.sign(self.z12_reg.data))
+            z12_add = self.z12.add(self.epsilon / self.pro_num * self.z12_reg.data)
         else:
             z12_add = self.z12
         a12 = self.features[39](self.features[38](z12_add)) #bn12,relu
 
-        self.z12 = self.features[40](a12)  # conv13
+        self.z13 = self.features[40](a12)  # conv13
         if self.enable_lat:
-            self.z12.retain_grad()
-            # LAT add saved grad to z12_reg
-            z12_add = self.z12.add(self.epsilon / self.pro_num * torch.sign(self.z12_reg.data))
+            self.z13.retain_grad()
+            # LAT add saved grad to z13_reg
+            z13_add = self.z13.add(self.epsilon / self.pro_num * self.z13_reg.data)
         else:
-            z12_add = self.z12
-        a13 = self.features[42](self.features[41](z12_add)) #bn12,relu
+            z13_add = self.z13
+        a13 = self.features[42](self.features[41](z13_add)) #bn12,relu
 
         p13 = self.features[43](a13) # maxpooling
 